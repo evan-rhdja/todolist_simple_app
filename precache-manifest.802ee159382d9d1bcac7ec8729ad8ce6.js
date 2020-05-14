b0d17f570ac7631cf659fef4dce1fec4f923bad0
eld16 <evan.rahardja97@gmail.com>
2020-05-14 15:48:36 +0700

    Updates

---
+++ precache-manifest.802ee159382d9d1bcac7ec8729ad8ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30a336099042c9f239ffad3c040646c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12943c8cfbeccb1ae6f0",
+    "url": "/static/css/main.14c702e8.chunk.css"
+  },
+  {
+    "revision": "95078947faaae61db7d1",
+    "url": "/static/js/2.012dabda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.012dabda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12943c8cfbeccb1ae6f0",
+    "url": "/static/js/main.2e465f8b.chunk.js"
+  },
+  {
+    "revision": "00b67eaf620f014cd0ac",
+    "url": "/static/js/runtime-main.13290678.js"
+  }
+]);
